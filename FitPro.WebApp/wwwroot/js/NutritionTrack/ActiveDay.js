2a5a5c11d38d43d38609e5400273cc8dedbd0a00
ruxandraapostol <58362868+ruxandraapostol@users.noreply.github.com>
2022-01-08 01:13:23 +0200

    - nutrition track controller without currentuser id

--- FitPro.WebApp/wwwroot/js/NutritionTrack/ActiveDay.js
+++ FitPro.WebApp/wwwroot/js/NutritionTrack/ActiveDay.js
@@ -1,15 +1,12 @@
 ﻿var activDayChange = function (event) {
     var date = $(event.currentTarget).data("date");
-    var idCurrentUser = $('#currentUserId').val();
 
     $.ajax({
         url: '/NutritionTrack/ChangeActiveDay',
         data: {
-            idRegularUser: idCurrentUser,
             date: date,
         }
     }).done(function () {
-        window.location.href = '/NutritionTrack/DailyTrack?idRegularUser=' + idCurrentUser
-            + '&date=' + date;
+        window.location.href = '/NutritionTrack/DailyTrack?date=' + date;
     });
 }
